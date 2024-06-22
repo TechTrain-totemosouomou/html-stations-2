ad90fcbfd7cbb3bec5822d9121b73abaa46e7091
totemosouomou <totemosouomou@users.noreply.github.com>
2024-06-22 19:58:43 +0900

    station13 clear

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,10 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+    var textElement = document.getElementById('text');
+    console.log(textElement);
+    if (this.checked) {
+      textElement.style.backgroundColor = 'red';
+    } else {
+      textElement.style.backgroundColor = '';
+    }
 }
